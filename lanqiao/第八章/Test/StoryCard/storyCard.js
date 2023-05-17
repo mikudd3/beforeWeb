541b889e33edaa3933ccbcd14020f32d4c394610
mikudd3 <96935176+mikudd3@users.noreply.github.com>
2023-05-17 10:52:23 +0800

    Add files via upload

---
+++ lanqiao/第八章/Test/StoryCard/storyCard.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#more").click(function(){
+        $(".pic_story").css("display","block");
+    })
+    $("#hide").click(function(){
+        $(".pic_story").css("display","none");
+    })
+})
